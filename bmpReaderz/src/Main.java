43176290b6283daab77415e98d4eaae80d06d1d4
bowlans <thomas.steiner03@gmail.com>
2019-07-14 13:01:53 -0700

    yd

---
+++ bmpReaderz/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+public class Main {
+public static void main(String[] args) throws IOException {
+Main main = new Main();
+System.out.println(main.getByte("test.bmp"));
+}
+public byte[] getByte(String bmpLoc) throws IOException {
+	ArrayList<Byte> ListData= new ArrayList<Byte>();
+	BufferedImage img = ImageIO.read(new File(bmpLoc));
+	
+	final int w = img.getWidth();
+	final int h = img.getHeight();
+	System.out.println(w+"/"+h);
+	byte[][][] pic = new byte[w][h][4];
+
+	//all 
+	byte[][] red = new byte[w][h];
+	byte[][] green = new byte[w][h];
+	byte[][] blue = new byte[w][h];
+	byte[][] alpha = new byte[w][h];
+	
+	for(int x=0; x<w; x++){
+	  for(int y=0; y<h; y++){
+	     int color = img.getRGB(x,y);
+	     alpha[x][y] = (byte)(color>>24);
+	     red[x][y] = (byte)(color>>16);
+	     green[x][y] = (byte)(color>>8);
+	     blue[x][y] = (byte)(color);
+	  }
+	}
+	//System.out.println(Byte.toUnsignedInt(red[0][0]));
+	for (int i = 0; i < w; i++) {
+		for (int j = 0; j < h; j++) {
+			
+				pic[j][i][0]=red[i][j];
+				pic[j][i][1]=green[i][j];
+				pic[j][i][2]=blue[i][j];
+				pic[j][i][3]=alpha[i][j];
+				
+		}
+	} 
+	
+//	for (int i = 0; i < w; i++) {
+//		for (int j = 0; j < h; j++) {
+//			pic[j][i][0]=red[i][j];
+//			pic[j][i][1]=green[i][j];
+//			pic[j][i][2]=blue[i][j];
+//			pic[j][i][3]=alpha[i][j];
+//			
+//		}
+//	}
+	int p=0;
+	byte[] turn = new byte[w*h*4];
+	for (byte[][] bs : pic) {
+		for (byte[] bs2 : bs) {
+			System.out.print("(");
+			for (byte bs3 : bs2) {
+				//System.out.print(Byte.toUnsignedInt(bs3)+",");
+				turn[p]=(byte)Byte.toUnsignedInt(bs3);
+				System.out.println(turn[p]);
+				p++;
+				
+			}
+			//System.out.print(")");
+		}
+		//System.out.print("\n");
+	}
+
+	
+			
+	return turn;
+	
+	
+}
+}
+//alpha(color>>24);
+//red(color>>16);
+//green(color>>8);
+//blue(color);

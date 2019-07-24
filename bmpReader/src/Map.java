43dc4e6290026747d08d9ef40f66face75790d47
League Student <league@iMac-7.attlocal.net>
2019-07-24 16:27:03 -0700

    base done
    100% done w/  base bmp to array
    50% done with bmpArray croping

--- bmpReader/src/Map.java
+++ bmpReader/src/Map.java
@@ -27,6 +27,7 @@ Map(String path) throws IOException{
 	byte[][] green = new byte[w][h];
 	byte[][] blue = new byte[w][h];
 	byte[][] alpha = new byte[w][h];
+	System.out.println("1");
 	//scans image rgba and isolates layers
 	for(int x=0; x<w; x++){
 	  for(int y=0; y<h; y++){
@@ -37,20 +38,23 @@ Map(String path) throws IOException{
 	     blue[x][y] = (byte)(color);
 	  }
 	}
-	
+	System.out.println("2");
+
 	//puts all layers into 1 3d array
 	//Key for pic:
 	//pic[x location of pixel][y loc of pixel][layer number(0=r,1=green,2=blue,3=alpha)]
 	for (int i = 0; i < w; i++) {
 		for (int j = 0; j < h; j++) {
 			
-				pic[j][i][0]=red[i][j];
-				pic[j][i][1]=green[i][j];
-				pic[j][i][2]=blue[i][j];
-				pic[j][i][3]=alpha[i][j];
+				pic[i][j][0]=red[i][j];
+				pic[i][j][1]=green[i][j];
+				pic[i][j][2]=blue[i][j];
+				pic[i][j][3]=alpha[i][j];
 				
 		}
 	} 
+	System.out.println("3");
+
 	//condenses 3d array into 1d array with 4x the size and organized linearly
 	int p=0;
 	for (byte[][] bs : pic) {
@@ -62,7 +66,8 @@ Map(String path) throws IOException{
 		}
 	}
 		
-	
+	System.out.println("4");
+
 }
 public void textDisplay() {
 	for (int i = 0; i < turn.length; i++) {
@@ -76,9 +81,57 @@ public void textDisplay() {
 		System.out.print(Byte.toUnsignedInt(turn[i])+",");
 	}
 }
+
+
 public byte[] cropTri(int x1,int y1,int x2,int y2,int x3,int y3){
-	return turn;
+	byte[][] visMap = new byte[width][height];
 	
+	for (int i = 0; i < width;i++) {
+		for (int j = 0; j < height; j++) {
+			visMap[i][j]=(byte) 255;
+			System.out.print(i+"/"+j+" ");
+		}
+		System.out.println();
+	}
+	visMap[x1][y1]=111;
+	visMap[x2][y2]=111;
+	visMap[x3][y3]=111;
+	
+
+	double slope12=(y1-y2)/(x1-x2);
+	
+	
+	double slope23=(y2-y3)/(x2-x3);
+	double slope13=(y3-y1)/(x3-x1);
+	System.out.println(slope13);
+	for (int i = 0; i < width; i++) {
+		if((int) (i+slope13*i)>0&&(int) (i+slope13*i)<width) {
+		visMap[i][(int) (i+slope13*i)]=111;
+		}
+	}
+	for (int i = 0; i < height; i++) {
+		if((int) (i+slope13*i)>0&&(int) (i+slope13*i)<width) {
+		visMap[(int) (i+slope13*i)][i]=111;
+		}
+	}
+//	for (int i = 0; i < width; i++) {
+//		if((int) (i+slope12*i)>0&&(int) (i+slope12*i)<width)
+//		visMap[i][(int) (i+slope12*i)]=111;
+//	}
+	//visual
+		for (int i = 0; i < visMap.length; i++) {
+			for (int j = 0; j < visMap.length; j++) {	
+				if(Byte.toUnsignedInt(visMap[i][j])==255) {
+					System.out.print("--- ");
+				}else {
+					System.out.print("### ");
+				}
+				//System.out.print(Byte.toUnsignedInt(visMap[i][j])+" ");
+			}
+			System.out.println("\n");
+		}
+		//vis
+	return turn;
 }
 
  }
